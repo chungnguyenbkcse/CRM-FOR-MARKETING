dcb82d833459957cfcdfb7bc72f50ca438c62099
chungnguyent2601 <chungnguyen10012000@gmail.com>
2023-05-10 13:49:28 +0700

    cấp nhập lại role lấy dữ liệu cho ro

--- custom/modules/Leads/update_data.php
+++ custom/modules/Leads/update_data.php
@@ -22,15 +22,23 @@ $spreadsheetId = '10qhcaru2svtbiLYmpZlMtsILS0HFbN7RZh7eMTtGs7M';
     [$bean->facebook_or_zalo_name, $bean->data_sources, $bean->phone_number_primary, $bean->card_bark_type, $bean->bank, $bean->payment_day, $bean->transaction_amount, $bean->fee, $bean->sale_stage, $bean->lead_status, $bean->owned_branch, $bean->ro_name, $bean->contact_date, $bean->note, $bean->fullname, $bean->citizen_identificationj, $bean->citizen_identification_issuance_date, $bean->citizen_identification_issuance_place, $bean->real_transaction_amount, $bean->real_fee, $bean->payment_day, $bean->birthday, $bean->successful_trading_day, $bean->phone_number_extra, $bean->career, $bean->district_customer_live]
 ]; */
 
+$filename = '/home/www/html/mkt.tranthu.vn/Book1.csv';
 
-$range = 'DATA CRM';
-$response = $service->spreadsheets_values->get($spreadsheetId, $range);
-$valuesx = $response->getValues();
-foreach ($valuesx as $row => $data) {
-    if ($row != 0) {
+// Mở tệp CSV để đọc
+$file = fopen($filename, 'r');
+
+// Kiểm tra xem tệp có tồn tại không
+if ($file) {
+    // Đọc dữ liệu từ tệp theo từng dòng
+    while (($data = fgetcsv($file)) !== false) {
+        // Lấy giá trị của cột đầu tiên
+        $firstColumnValue = '0' . $data[0];
+        
+        // Xử lý giá trị của cột đầu tiên
+        // Ví dụ: In ra giá trị của cột đầu tiên
         $lst = array();
 
-        $query = "SELECT * FROM leads WHERE phone_number_primary = '{$data[3]}'";
+        $query = "SELECT * FROM leads WHERE phone_number_primary = '{$firstColumnValue}'";
         $result =  $GLOBALS['db']->query($query);
         while ($rows = $GLOBALS['db']->fetchByAssoc($result)) {
             $lead_status_follow_levels = array(
@@ -108,28 +116,39 @@ foreach ($valuesx as $row => $data) {
             else {
                 $lst[1] = 'Không có dữ liệu';
             }
+
+            $values = [
+                $lst
+            ];
     
-            
+    
+            $body = new Google_Service_Sheets_ValueRange([
+                'values' => $values
+            ]);
+    
+            $params = [
+                'valueInputOption' => 'RAW'
+            ];
+    
+            $range = 'DATA CRM';
+            $response = $service->spreadsheets_values->get($spreadsheetId, $range);
+            $values = $response->getValues();
+            $key = $rows['phone_number_primary'];
+
+            foreach ($values as $row => $datax) {
+                if ($datax[3] == substr($key, 1)) {
+                    //$rangeToUpdate = 'DATA CRM!A' . ($row + 1) . ':Z' . ($row + 1);
+                    $rangeToUpdate = 'DATA CRM!AA' . ($row + 1) . ':AB' . ($row + 1);
+                    $result = $service->spreadsheets_values->update($spreadsheetId, $rangeToUpdate, $body, $params);
+                    break;
+                }
+            }
     
         }
-    
-        $values = [
-            $lst
-        ];
-    
-    
-        $body = new Google_Service_Sheets_ValueRange([
-            'values' => $values
-        ]);
-    
-        $params = [
-            'valueInputOption' => 'RAW'
-        ];
-    
-    
-        $rangeToUpdate = 'DATA CRM!AA' . ($row + 1) . ':AB' . ($row + 1);
-        $result = $service->spreadsheets_values->update($spreadsheetId, $rangeToUpdate, $body, $params);
-    }
-    
 
+    }
+    // Đóng tệp CSV sau khi đọc xong
+    fclose($file);
+} else {
+    echo "Không thể mở tệp CSV!";
 }
--- custom/modules/Leads/update_data_1.php
+++ custom/modules/Leads/update_data_1.php
@@ -1,5 +1,5 @@
 <?php
-$filename = '/home/www/html/mkt.tranthu.vn/nguyen_pttn.csv';
+$filename = '/home/www/html/mkt.tranthu.vn/DATA_IMPORT_DOI_TAC.csv';
 
 // Mở tệp CSV để đọc
 $file = fopen($filename, 'r');
@@ -7,14 +7,56 @@ $file = fopen($filename, 'r');
 // Kiểm tra xem tệp có tồn tại không
 if ($file) {
     // Đọc dữ liệu từ tệp theo từng dòng
+    $i = 0;
     while (($data = fgetcsv($file)) !== false) {
         // Lấy giá trị của cột đầu tiên
-        $firstColumnValue = $data[0];
+        $contact_date = $data[0];
+        $data_sources = $data[1];
+
+        $phone_number_primary = $data[2];
+        $service = $data[3];
+        $bank = $data[4];
+        $payment_day = $data[5];
+        $transaction_amount = $data[6];
+        $fee = (int)$data[7] * 100;
+        $sale_stage = $data[8];
+        $lead_status = $data[9];
+        $owned_branch = $data[10];
+        $ro_name = $data[11];
+        $fullname = $data[12];
+
+        $query_1 = "SELECT COUNT(*) AS total  FROM leads WHERE phone_number_primary = '{$phone_number_primary}' AND bank = '{$bank}' AND ro_name = '{$ro_name}' ";
+        $result_1 = $GLOBALS['db']->query($query_1);
+        $row_1 = $GLOBALS['db']->fetchByAssoc($result_1);
+        $total_1 = $row_1['total'];
+
+        if ($total_1 == 0) {
+            echo 'hello';
+            // Xử lý giá trị của cột đầu tiên
+            // Ví dụ: In ra giá trị của cột đầu tiên
+            $key = 'partner-' . $i;
+            $query = "INSERT INTO leads 
+            (id, contact_date, data_sources, phone_number_primary, service, bank, payment_day, transaction_amount, fee, sale_stage, lead_status, owned_branch, ro_name, fullname) 
+            VALUES (
+                '{$key}',
+                '{$contact_date}',
+                '{$data_sources}',
+                '{$phone_number_primary}',
+                '{$service}',
+                '{$bank}',
+                '{$payment_day}',
+                '{$transaction_amount}',
+                '{$fee}',
+                '{$sale_stage}',
+                '{$lead_status}',
+                '{$owned_branch}',
+                '{$ro_name}',
+                '{$fullname}'
+            );";
+            $GLOBALS['db']->query($query);
+        }
         
-        // Xử lý giá trị của cột đầu tiên
-        // Ví dụ: In ra giá trị của cột đầu tiên
-        $query = "UPDATE leads SET data_sources = '15' WHERE phone_number_primary = '{$firstColumnValue}'";
-        $GLOBALS['db']->query($query);
+        $i = $i + 1;
     }
     // Đóng tệp CSV sau khi đọc xong
     fclose($file);
